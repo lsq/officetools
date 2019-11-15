4fd609859ca472e677bb70877ffb4d4ba43472f2
lsq <lsqypj@gmail.com>
2019-11-15 19:26:06 +0800

    modify appveyor settings

--- win32ole-excel.rb
+++ win32ole-excel.rb
@@ -2,15 +2,12 @@
 require 'WIN32OLE'
 
 excel = WIN32OLE::new('excel.application')
-puts ".... WIN32OLE: #{excel.class}: #{excel}"
 #excel.visible =true
 wb = excel.workbooks.add
-puts 'add workbook...'
 sheet = wb.sheets(1)
 sheet.cells(1,1).value = "abc|123.3"
 sheet.Cells(2,1).value = "cdb|789.44"
 sheet.Cells(3,1).value = "2019-04-05|lakbae"
-puts 'add data'
 sheet.range("A:A").select
 sheet.range("A:A").TextToColumns(:Destination => sheet.range("E1"), :DataType => 1, :TextQualifier => 1, 
 						  :ConsecutiveDelimiter => false, 
@@ -20,4 +17,3 @@ sheet.range("A:A").TextToColumns(:Destination => sheet.range("E1"), :DataType =>
 #
 puts 'ok.....'
 wb.saveas('C:\Users\appveyor\lsqypj.xls', :FileFormat => -4143) # 56 for xls, 51 for xlsx, more info https://docs.microsoft.com/zh-cn/office/vba/api/excel.xlfileformat
-puts 'xls gen....'
