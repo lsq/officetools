851bfdb987f5ed52a696626bebbc97f8afa1a381
lsq <lsqypj@gmail.com>
2019-11-15 14:47:44 +0800

    add win32ole test

---
+++ appveyor_prepare.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+PKG_PREFIX="mingw-w64-$MSYS2_ARCH"
+pacman -S --noconfirm --needed base-devel\
+       ${PKG_PREFIX}-json-c \
+       ${PKG_PREFIX}-glib2 \
+       ${PKG_PREFIX}-gobject-introspection\
+	   bash pacman msys2-runtime
+pacman -S --noconfirm git subversion mingw64/mingw-w64-x86_64-ruby\
+	   mingw64/mingw-w64-x86_64-libxslt python3 python\
+	   mingw64/mingw-w64-x86_64-sqlite3 msys/libsqlite-devel unzip
+
+# 
+echo ---------------
+# echo %path%
+#which set
+#echo -----set-------
+#set  | grep access_token
+#which env
+#echo ++++++env++++++
+#env | grep access_token
+#echo $PATH
+#cat >~/.git-credentials <<EOF
+#https://$access_token:x-oauth-basic@github.com
+#EOF
+#echo ---------------
+#bash -x get_hugo.sh
+## which hugo && /usr/bin/hugo -t hugo-theme-den --baseUrl="https://github.com/lsq/lsq.github.io"
+#mkdir $APPVEYOR_JOB_ID
+##bash -x get_latest.sh 2dust/v2rayNG v2rayNG_1.1.9_arm64-v8a.apk
+#bash -x get_latest.sh src/readme content/posts/downloaded.md
---
+++ win32ole-excel.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby -w 
+require 'WIN32OLE'
+
+excel = WIN32OLE::new('excel.application')
+excel.visible =true
+wb = excel.workbooks.add
+sheet = wb.sheets(1)
+sheet.cells(1,1).value = "abc|123.3"
+sheet.Cells(2,1).value = "cdb|789.44"
+sheet.Cells(3,1).value = "2019-04-05|lakbae"
+sheet.range("A:A").select
+sheet.range("A:A").TextToColumns(:Destination => sheet.range("E1"), :DataType => 1, :TextQualifier => 1, 
+						  :ConsecutiveDelimiter => false, 
+						  :Tab=> true, :Semicolon => false, :Comma => false, :Space => false, :Other => true, :OtherChar => '|',
+ 						  :FieldInfo=>[[1,1],[2,2]], :TrailingMinusNumbers => true)						  
+#						  :FieldInfo=>'Array(Array(1, 1), Array(2, 2))', :TrailingMinusNumbers=>true)
+wb.saveas('C:\Users\Administrator\lsqypj.xls', :FileFormat => -4143) # 56 for xls, 51 for xlsx, more info https://docs.microsoft.com/zh-cn/office/vba/api/excel.xlfileformat
