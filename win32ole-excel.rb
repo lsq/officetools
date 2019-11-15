5b099849dde9fd92d8f44ffe1187518e97a6c1b1
lsq <lsqypj@gmail.com>
2019-11-15 16:37:52 +0800

    modify win32ole test

--- win32ole-excel.rb
+++ win32ole-excel.rb
@@ -2,16 +2,22 @@
 require 'WIN32OLE'
 
 excel = WIN32OLE::new('excel.application')
+puts '.... WIN32OLE'
 #excel.visible =true
 wb = excel.workbooks.add
+puts 'add workbook...'
 sheet = wb.sheets(1)
 sheet.cells(1,1).value = "abc|123.3"
 sheet.Cells(2,1).value = "cdb|789.44"
 sheet.Cells(3,1).value = "2019-04-05|lakbae"
+puts 'add data'
 sheet.range("A:A").select
 sheet.range("A:A").TextToColumns(:Destination => sheet.range("E1"), :DataType => 1, :TextQualifier => 1, 
 						  :ConsecutiveDelimiter => false, 
 						  :Tab=> true, :Semicolon => false, :Comma => false, :Space => false, :Other => true, :OtherChar => '|',
  						  :FieldInfo=>[[1,1],[2,2]], :TrailingMinusNumbers => true)						  
 #						  :FieldInfo=>'Array(Array(1, 1), Array(2, 2))', :TrailingMinusNumbers=>true)
+#
+puts 'ok.....'
 wb.saveas('lsqypj.xls', :FileFormat => -4143) # 56 for xls, 51 for xlsx, more info https://docs.microsoft.com/zh-cn/office/vba/api/excel.xlfileformat
+puts 'xls gen....'
