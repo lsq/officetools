e3d193cf0c824c7d7dcbc734a3d92277aa508c6f
lsq <lsqypj@gmail.com>
2019-12-04 08:14:45 +0800

    Update dl.ps1

--- dl.ps1
+++ dl.ps1
@@ -17,7 +17,8 @@
     [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";C:\msys64\mingw64\bin;C:\msys64\usr\bin",
     [EnvironmentVariableTarget]::Machine)
 #
-$env:Path +=  ";C:\msys64\mingw64\bin;C:\msys64\usr\bin"
+$currentpath = "$(pwd | Select-String \\)"
+$env:Path +=  ";C:\Program Files\7-Zip;C:\msys64\mingw64\bin;C:\msys64\usr\bin;$currentpath"
 $filename="$(Get-Date -Format 'yyyyMdHMs').txt"
 echo $env:Path > $filename
 #curl --version
@@ -26,10 +27,20 @@ $msys2='msys2-x86_64-20190524.exe'
 $msbash='C:\msys64\usr\bin\bash.exe'
 $pacmanset='pacmanset.sh'
 $installtool='installtools.sh'
-wget -Uri 'https://mirrors.cqu.edu.cn/CTAN/systems/texlive/tlnet/tlpkg/installer/wget/wget.exe' -OutFile 'wget.exe'
-wget  -uri 'https://curl.haxx.se/windows/dl-7.67.0_4/curl-7.67.0_4-win64-mingw.zip' -OutFile 'curl.zip'
-#.\wget.exe https://curl.haxx.se/windows/dl-7.67.0_4/curl-7.67.0_4-win64-mingw.zip
-Expand-Archive .\curl.zip -DestinationPath .
+$client.DownloadFile('https://mirrors.cqu.edu.cn/CTAN/systems/texlive/tlnet/tlpkg/installer/wget/wget.exe', "$currentpath\wget.exe")
+#wget -Uri 'https://mirrors.cqu.edu.cn/CTAN/systems/texlive/tlnet/tlpkg/installer/wget/wget.exe' -OutFile 'wget.exe'
+#$client.DownloadFile('https://curl.haxx.se/windows/dl-7.67.0_4/curl-7.67.0_4-win64-mingw.zip', 'curl.zip')
+#wget  -uri 'https://curl.haxx.se/windows/dl-7.67.0_4/curl-7.67.0_4-win64-mingw.zip' -OutFile 'curl.zip'
+wget.exe -c -t 3 https://www.7-zip.org/a/7z1900-x64.exe
+.\7z1900-x64.exe
+.\wget.exe -c -t 3 ftp://ftp.info-zip.org/pub/infozip/win32/unz600xn.exe
+.\unz600xn.exe -o
+.\wget.exe -c -t 3 -d --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" --header="Referer: https://curl.haxx.se/windows/dl-7.67.0_4/" https://curl.haxx.se/windows/dl-7.67.0_4/curl-7.67.0_4-win64-mingw.zip
+# .\wget.exe -c -t 3 https://curl.haxx.se/windows/dl-7.67.0_4/curl-7.67.0_4-win64-mingw.zip -o curl.zip
+# powershell 2.0 not support 
+# Expand-Archive .\curl.zip -DestinationPath .
+.\unzip.exe -o .\curl*.zip
+(test-path curl) -and ( rm -Force -Recurse .\curl)
 mv curl-7.* curl
 $env:Path +=  ";.\curl\bin"
 #$client.DownloadFile('https://mirrors.cqu.edu.cn/CTAN/systems/texlive/tlnet/tlpkg/installer/wget/wget.exe','wget.exe')
@@ -39,11 +50,25 @@ curl.exe -sLS -o $msys2 http://mirrors.ustc.edu.cn/msys2/distrib/x86_64/msys2-x8
 # $client.DownloadFile('http://iso.mirrors.ustc.edu.cn/msys2/distrib/x86_64/msys2-x86_64-20190524.exe', $msys2)
 (Test-Path $msys2)  -and (Start-Process .\$msys2 -wait)
 if(Test-Path $msbash) { echo "bash in path..." }else{ exit }
-curl.exe  -s https://raw.githubusercontent.com/lsq/officetools/master/tools/pacmanset.sh -o $pacmanset
-curl.exe -s https://raw.githubusercontent.com/lsq/officetools/master/tools/installtoos.sh -o $installtool
+# curl -f -S conflict
+curl.exe -sLSO "https://download-ssl.firefox.com.cn/releases-sha2/stub/official/zh-CN/Firefox-latest.exe"
+(Test-Path Firefox-latest.exe)  -and (Start-Process .\Firefox-latest.exe )
+curl.exe -sLSO  "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B20C5A533-0647-7599-741A-D8AD97BBC271%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Ddefaultbrowser/update2/installers/ChromeSetup.exe"
+(Test-Path ChromeSetup.exe)  -and  (Start-Process .\ChromeSetup.exe )
+curl.exe -sSLO https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-win64.zip
+unzip.exe -o geckodriver-v0.26.0-win64.zip
+cp geckodriver*.exe C:\Windows\System32
+curl.exe  -vSL https://raw.githubusercontent.com/lsq/officetools/master/tools/pacmanset.sh -o $pacmanset
+curl.exe -vSL https://raw.githubusercontent.com/lsq/officetools/master/tools/installtoos.sh -o $installtool
 #(Test-Path $msys2)  -and (cmd /c $msys2)
-if((Test-Path $msbash) -and (Test-Path $pacmanset) ){ cmd /c $msbash -x $pacmanset  }else{ exit }
-cmd /c $msbash pacman -Syu
+if((Test-Path $msbash) -and (Test-Path $pacmanset) ){ cmd.exe /c $msbash -x $pacmanset  }else{ exit }
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-6
+# Start-Process -FilePath "$env:comspec" -ArgumentList "/c dir `"%systemdrive%\program files`""
+# Start-Process -FilePath "$env:comspec" -ArgumentList "/c", $msbash, "-c `"pacman -Syu`"" -NoNewWindow -Wait
+Start-Process -FilePath "$env:comspec" -ArgumentList "/c", $msbash, "-c `"pacman -Syu`""  -Wait
+Start-Process -FilePath "$env:comspec" -ArgumentList "/c", $msbash, "-c `"pacman -Syu`""  -Wait
+Start-Process -FilePath "$env:comspec" -ArgumentList "/c", $msbash, "-x $installtool"  -Wait
+
 
 
 # (Test-Path C:\msys64\usr\bin\bashs.exe) -and (echo $a)
