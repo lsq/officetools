842c1c3f989ec7ee45736044e86af21b4c2a4f01
lsq <lsqypj@gmail.com>
2019-12-05 22:55:37 +0800

    Update office_build.sh

--- office_build.sh
+++ office_build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+curl -sOL https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_12130-20272.exe
+ls -al
+7z x officedeploymenttool_12130-20272.exe
+setup.exe /download o2019.xml
+ls -al
+df -h
+setup.exe /configure o2019.xml
+
+
 cd $APPVEYOR_BUILD_FOLDER
 type gem
 gem install pry
