413100b42e786405d186e08bbe23cf85d5cad0dc
lsq <lsqypj@gmail.com>
2019-12-06 03:25:18 +0800

    Update office_build.sh

--- office_build.sh
+++ office_build.sh
@@ -3,11 +3,13 @@
 curl -sOL https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_12130-20272.exe
 ls -al
 7z x officedeploymenttool_12130-20272.exe
-cmd //c setup.exe /download o2019.xml
+cat >installOffice.bat <<EOF
+setup.exe /download o2019.xml
+setup.exe /configure o2019.xml
+EOF
+cmd //c installOffice.bat
 ls -al
 df -h
-cmd //c setup.exe /configure o2019.xml
-
 
 cd $APPVEYOR_BUILD_FOLDER
 type gem
