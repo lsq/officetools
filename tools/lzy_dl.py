bf74bd1b9e4c8a0c97837e95232d9990c65613a1
lsq <lsqypj@gmail.com>
2019-12-06 09:03:41 +0800

    Update lzy_dl.py

--- tools/lzy_dl.py
+++ tools/lzy_dl.py
@@ -22,16 +22,23 @@ url2 = 'https://www.lanzous.com/'+soup.find('iframe')['src']
 res2 = requests.get(url2,headers=headers)
 
 # 正则提取请求三个参数
-a = re.findall(r'var a = \'([\w]+?)\';',res2.text)
-params = re.findall(r'var [\w]{6} = \'([\w]+?)\';',res2.text)
+#a = re.findall(r'var a = \'([\w]+?)\';',res2.text)
+#params = re.findall(r'var [\w]{6} = \'([\w]+?)\';',res2.text)
+params = {'action':'', 'sign':'', 'ves':''}
+params['action'] = re.findall(r'\'action\':\'([\w]+?)\',', res2.text)
+params['sign'] = re.findall(r'\'sign\':\'([\w]+?)\',', res2.text)
+params['ves'] = re.findall(r'\'ves\':\'([\w]+?)\'', res2.text)
+#print(len(params))
 
 # 请求下载地址
 url3 = 'https://www.lanzous.com/ajaxm.php'
 data = {
-    'action':'down_process',
-    'file_id':params[0],
-    't':params[1],
-    'k':params[2],
+    'action': params['action'],
+    'sign': params['sign'],
+    'ves': params['ves'],
+    #'file_id':params[0],
+    #'t':params[1],
+    #'k':params[2],
 }
 res3 = requests.post(url3,headers=headers,data=data)
 res3 = json.loads(res3.content)
@@ -43,11 +50,10 @@ headers2 = {
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
 }
 res4 = requests.head(url4, headers=headers2)
-print res4.headers['Location']
-
-
+print(res4.headers['Location'])
 
 # 作者：伤心童话
 # 链接：https://juejin.im/post/5cc34961e51d456e6e389290
 # 来源：掘金
 # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
