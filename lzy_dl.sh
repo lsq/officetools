43f19e5c28a4cddd580379ac5d4e244b8b52f70d
lsq <lsqypj@gmail.com>
2019-12-06 09:11:16 +0800

    Update lzy_dl.sh

--- lzy_dl.sh
+++ lzy_dl.sh
@@ -4,4 +4,5 @@ which python
 pip3 install b4 requests 
 
 cd $APPVEYOR_BUILD_FOLDER/$APPVEYOR_JOB_ID
-python3 $APPVEYOR_BUILD_FOLDER/tools/lzy_dl.py
+url=$(python3 $APPVEYOR_BUILD_FOLDER/tools/lzy_dl.py)
+wget -c --content-disposition "$url"
