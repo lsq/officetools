3c31497a667e83aab9916eea73229e8cc51a850e
lsq <lsqypj@gmail.com>
2019-12-08 07:02:35 +0800

    Update mechanize_lzy.rb

--- tools/mechanize_lzy.rb
+++ tools/mechanize_lzy.rb
@@ -1,39 +1,43 @@
 require 'mechanize'
+require 'pry'
 agent = Mechanize.new
+agent.user_agent_alias = 'Windows Mozilla'
 page = agent.get('https://up.woozooo.com/account.php?action=login&ref=/mydisk.php')
 page.form_with(name:'user_form')
 loginform = page.form_with(name:'user_form')
+if File.exist?("cookies.yaml")
+  #agent.cookie_jar.load("cookies.yaml")
+  puts "rewrite now...."
+# else
 loginform.username = '13113602265'
 loginform
 loginform.password = 'x'
 loginform
-loginpage = agent.submit(loginform,loginform.button_with(value:'登 陆')
-)
-loginpage.link_with(text:/Lanzou/)
-loginpage.link_with(text:/Lanzou/).click
-loginpage
-loginpage.link_with(text:/'登录'/).click
-loginpage.link_with(text:/登录/).click
-loginpage
-mainframe = loginpage.link_with(text:/登录/).click.frame('mainframe')
-mainframe
-loginpage.link_with(text:/登录/).click.frame('mainframe')
-loginpage
-loginpage.link_with(text:/登录/).click
-loginpage.link_with(text:/登录/).click.frame('mainframe')
-loginpage.link_with(text:/登录/).click.iframe('mainframe')
+loginpage = agent.submit(loginform,loginform.button_with(value:'登 陆'))
+end
+
+# screen-scrapey stuff
+#agent.cookie_jar.save("cookies.yaml")
+#But you can override this behavior with an option:
+
+agent.cookie_jar.save("cookies.yaml", session: true)
+#agent.cookie_jar.save("cookies.yaml", session: true)
+# With the session flag set, Mechanize will write out the full contents of its cookie jar for later loading.
+#puts loginpage.link_with(text:/Lanzou/)
 mainframe = loginpage.link_with(text:/登录/).click.iframe('mainframe')
-mainframe.click
-mainframe.click.iframe('mainframe').click
 rootfile = mainframe.click.iframe('mainframe').click
 rtfile_form = rootfile.form('file_form')
-rtfile_form = rootfile.form('file_form').click
-rtfile_form = rootfile.form('file_form').link
-rtfile_form = rootfile.form('file_form').links
-rtfile_form = rootfile.form('file_form').action
-rtfile_form = rootfile.form('file_form').action.click
-rtfile_form
+puts rtfile_form
 rtfile_form = rootfile.form('file_form')
 host= 'https://pc.woozooo.com/'
-agent.post(host+"/"+ rtfile_form.action, {item: 'files', action: 'index', u:'1311360225'})
+# I could solve this by adding the cookies before submitting the form.
+# cookie = Mechanize::Cookie.new(name: '#{name}', value: '#{value}', domain: '#{domain}', path: '/')
+# agent.cookie_jar.load(cookie)
+
+# https://stackoverflow.com/questions/18593380/store-login-session-cookie-in-browser-using-ruby-mechanize
+# agent.post(host+"/"+ rtfile_form.action, {item: 'files', action: 'index', u:'1311360225'})
+agent.cookie_jar.load("cookies.yaml")
 agent.post(host+"/"+ rtfile_form.action, { action: 'index', u:'1311360225'})
+
+binding.pry
+puts "finished!!"
