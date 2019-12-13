2decbfc4e6afb8f429b534c6fce90be97e82369d
lsq <lsqypj@gmail.com>
2019-12-13 15:36:41 +0800

    Update curl_gecko.bat

--- tools/curl_gecko.bat
+++ tools/curl_gecko.bat
@@ -3,6 +3,6 @@ ls -alh
 firefox.exe --version
 which geckodriver
 start cmd.exe /k "geckodriver.exe -vv "
-curl -vd '{"capabilities": {"alwaysMatch": {"acceptInsecureCerts": true}}}' http://localhost:4444/session
+curl -vd "{\"capabilities\": {\"alwaysMatch\": {\"acceptInsecureCerts\": true}}}" http://localhost:4444/session
 pwd
 ls -alh
