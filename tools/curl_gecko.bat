4e567d03d8453e3d85e65755f099d9295e627768
lsq <lsqypj@gmail.com>
2019-12-13 15:56:20 +0800

    Update and rename curl_gecko.bat to curl_gecko.sh

--- tools/curl_gecko.bat
+++
@@ -1,8 +0,0 @@
-cd "%ProgramFiles%\Mozilla Firefox\"
-ls -alh
-firefox.exe --version
-which geckodriver
-start cmd.exe /k "geckodriver.exe -vv "
-curl -vd "{\"capabilities\": {\"alwaysMatch\": {\"acceptInsecureCerts\": true}}}" http://localhost:4444/session
-pwd
-ls -alh
---
+++ tools/curl_gecko.sh
@@ -0,0 +1,10 @@
+env
+cd "%ProgramFiles%\Mozilla Firefox\"
+ls -alh
+firefox.exe --version
+which geckodriver
+cmd //c start cmd.exe /k "geckodriver.exe -vv "
+# curl -vd "{\"capabilities\": {\"alwaysMatch\": {\"acceptInsecureCerts\": true}}}" http://localhost:4444/session
+curl -v -d '{"capabilities": {"alwaysMatch": {"acceptInsecureCerts": true,"moz:firefoxOptions": {"args": ["-headless"], "prefs": {"dom.ipc.processCount": 8}, "log":{"level": "trace"}}}}}' http://localhost:4444/session
+pwd
+ls -alh
