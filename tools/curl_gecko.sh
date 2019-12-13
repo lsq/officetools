718edbd67c38fdee69c2e0ccd72487b86d8019df
lsq <lsqypj@gmail.com>
2019-12-13 14:35:37 +0800

    Update curl_gecko.sh

--- tools/curl_gecko.sh
+++ tools/curl_gecko.sh
@@ -1,3 +1,7 @@
+cd $ProgramFiles/Mozilla\ Firefox/
+ls -alh
+firefox.exe --version
+which geckodriver
 cmd.exe //c geckodriver.exe -vv &
 curl -vd '{"capabilities": {"alwaysMatch": {"acceptInsecureCerts": true}}}' http://localhost:4444/session
 ls -alh
