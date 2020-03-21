6cc0bc372db9af93fb9d45bd5a6496a7bdb8fe9f
lsq <lsqypj@gmail.com>
2020-03-21 20:21:16 +0800

    k3cloude download bom list

---
+++ tools/k3.rb
@@ -0,0 +1,52 @@
+require 'watir'
+b = Watir::Browser.new :firefox
+b.goto 'https://bgsdc.ik3cloud.com/k3cloud/html5/index.aspx'
+b.text_field(id: 'user').exists?
+b.text_field(id: 'user').set '刘军晓'
+b.text_field(id: 'password').set 'lsq!1213BDM'
+b.text_field(id: 'velidationCode').set 'cfg4'
+b.button(id:'btnLogin').click
+b.spans.text_field('物料清单成本查询').exists?
+b.text_field('物料清单成本查询').exists?
+b.span(class:'kd-funcText-color').text
+b.span(class:'kd-funcText-color').texts
+b.spans(class:'kd-funcText-color').texts
+b.spans(class:'kd-funcText-color').text
+b.spans(class:'kd-funcText-color')
+b.element(xpath: "/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[1]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[1]/div/div[2]/div/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/div[1]/div/div[2]/div/div[1]/div[4]/div/a/span[1]/img").click
+b.input(class:'3BDMi9N8V130
+').exists?
+b.input(class:'k-input').exists?
+b.input(class:'k-input').set '3BDMi9N8V130
+'
+b.input(class:'k-input').set '3BDMi9N8V130'
+b.input(class:'k-input').text = '3BDMi9N8V130'
+b.input(class:'k-input').text
+b.input(class:'k-input').text = '3BDMi9N8V130
+'
+b.input(class:'k-input').text = '3BDMi9N8V130'
+b.text_field(class:'k-input').exists?
+b.text_field(class:'k-input').set '3BDMi9N8V130
+'
+b.text_field(class:'k-input').set '3BDMi9N8V130'
+b.text_field(class:'k-input')
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').exists?
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').set '3BDM'
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').text
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').set 'xx'
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').send 'xx'
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').set('xx')
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').click
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').set 'xx'
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').to_subtype.set('xx')
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').to_subtype
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').to_subtype.set('i3xp2')
+b.element(xpath:'/html/body/form/div[1]/div/div/div/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/span/span/input').to_subtype.set('3BDMi9N8V131')
+b.a(text:'刷新').exists?
+b.a(text:'刷新').click
+b.label(title:'BOM层级显示序号').exists?
+b.label(text:'BOM层级显示序号').exists?
+b.label(text:'BOM层级显示序号').click
+b.span(text:'引出').exists?
+b.span(text:'引出').click
+b.a(text:'下载').href
