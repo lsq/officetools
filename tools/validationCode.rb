70f9110353cfe160ff2e62dcb342b3743916ef73
lsq <lsqypj@gmail.com>
2020-04-13 20:43:04 +0800

    Update validationCode.rb

--- tools/validationCode.rb
+++ tools/validationCode.rb
@@ -13,17 +13,21 @@ wdku_uri = 'https://ocr.wdku.net/Upload'
 
 # uri = URI(wdku_uri)
 
-c = Curl::Easy.new(wdku_uri)
-c.multipart_form_post = true
+# c = Curl::Easy.new(wdku_uri)
+# c.multipart_form_post = true
 # if c.http_post(Curl::PostField.file('file', 'd.jpg')) then
 	# upload_info = JSON.parse(c.body_str)
 # end
-upload_info = c.http_post(Curl::PostField.file('file', 'd.jpg')) do |curl|
-	puts curl.class
-	curl.on_success do |upload_code|
-		puts "上传成功！"
+upload_info = Curl::Easy.new(wdku_uri) do |curl|
+  curl.multipart_form_post = true
+  curl.http_post(Curl::PostField.file('file', 'd.jpg')) 
+  puts curl.class
+  #curl.on_body {|body_data| puts "success, the body data are #{body_data}!"}
+  puts curl.body_str
+	#curl.on_success do |upload_code|
+	#	puts "上传成功！"
 		@upload_id = JSON.parse(curl.body_str)["data"]["id"]
-	end
+	#end
 end
 	
 puts upload_info
