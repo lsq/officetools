311275596e503df080cd68ef18985a91b1da7e4d
lsq <lsqypj@gmail.com>
2024-08-24 21:50:14 +0800

    Update scoop_vim_install.ps1

--- tools/scoop_vim_install.ps1
+++ tools/scoop_vim_install.ps1
@@ -1,7 +1,10 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
-scoop bucket add extras games nerd-fonts nirsoft sysinternals java nonportable php versions
-scoop install aria2 curl grep sed less touch vim-nightly
+"extras games nerd-fonts nirsoft sysinternals java nonportable php versions" -split '\s+' |`
+    {
+        scoop bucket add "$_"
+    }
+scoop install aria2 curl grep sed less touch vim-nightly ctags global
