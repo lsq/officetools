a7381be981cf22f0f3bf30da7dbce2c6fea792c8
lsq <lsqypj@gmail.com>
2024-08-27 09:25:46 +0800

    Update scoop_vim_install.ps1

--- tools/scoop_vim_install.ps1
+++ tools/scoop_vim_install.ps1
@@ -8,4 +8,11 @@ iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
         scoop bucket add "$_"
     }
 scoop install aria2 curl grep sed less touch vim-nightly ctags global
-svn checkout https://github.com/msys2/MSYS2-packages/trunk/vim
+function downGit($repo, $folder){
+    $json = irm https://api.github.com/repos/$repo/contents/$folder?ref=master
+    $json | ForEach-Object {
+        echo $_.path
+        iwr -useb $_.url | ni $_.path -Force
+    }
+}
+downGit "msys2/MSYS2-packages" "vim"
