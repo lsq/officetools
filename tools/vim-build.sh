7412e1d04c3771f631ad0ec5f31b16b80265f653
lsq <lsqypj.gmail.com>
2024-08-29 01:40:15 +0800

    update build info

--- tools/vim-build.sh
+++ tools/vim-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 set -x
 printf "$MSYSTEM"
-cd ./vim
-MINGW_ARCH=msys makepkg-mingw --cleanbuild --syncdeps --force --noconfirm
+#cd ./vim
+#MINGW_ARCH=msys makepkg-mingw --cleanbuild --syncdeps --force --noconfirm
 cd $APPVEYOR_BUILD_FOLDER/tools/vim
-export LUA_PREFIX=/ucrt64
+#export LUA_PREFIX=/ucrt64
 MINGW_ARCH=ucrt64 makepkg-mingw --cleanbuild --syncdeps --force --noconfirm
