63c56e3c70b75a014a670619a268f6b751f32da4
lsq <lsqypj.gmail.com>
2024-08-30 01:32:31 +0800

    update build script

--- tools/vim-build.sh
+++ tools/vim-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 set -x
 printf "$MSYSTEM"
+pacman --noconfirm --sync --needed pactoys
+pacman-key --recv-keys BE8BF1C5
+pacman-key --lsign-key BE8BF1C5
+repman add ci.ri2 "https://github.com/oneclick/rubyinstaller2-packages/releases/download/ci.ri2"
 #cd ./vim
 #MINGW_ARCH=msys makepkg-mingw --cleanbuild --syncdeps --force --noconfirm
 cd $APPVEYOR_BUILD_FOLDER/tools/vim
@@ -12,4 +16,12 @@ export PATH=$rubyhome/bin:$PATH
 ridk.cmd install
 echo $PATH
 ls $rubyhome/bin/
+rbpat=$(which ruby)
+rbdir=${rbpat%/*}
+rubyhm=${rbdir%/*}
+rubyversion=$(ruby -v | sed -r -n 's/.* (([0-9]{1,2})\.([0-9]{1,2})\.)[0-9]{1,2} .*/\2\3/p')
+rubyapiver=$(ruby -v | sed -r -n 's/.* (([0-9]{1,2})\.([0-9]{1,2})\.)[0-9]{1,2} .*/\10/p')
+sed -i "s|RUBY=\${ruby_home}|RUBY=${rubyhm}|" $APPVEYOR_BUILD_FOLDER/tools/vim/PKGBUILD
+sed -i "s|RUBY_VER=32|RUBY_VER=${rubyversion}" $APPVEYOR_BUILD_FOLDER/tools/vim/PKGBUILD
+sed -i "s|RUBY_API_VER_LONG=3.2.0|RUBY_API_VER_LONG=${rubyapiver}" $APPVEYOR_BUILD_FOLDER/tools/vim/PKGBUILD
 MINGW_ARCH=ucrt64 makepkg-mingw -L --cleanbuild --syncdeps --force --noconfirm
