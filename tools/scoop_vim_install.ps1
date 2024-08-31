1cba00ce30b14a8ac65c928158569d5aa3c6d905
lsq <lsqypj.gmail.com>
2024-09-01 04:27:01 +0800

    racket touch bc_suffix.h

--- tools/scoop_vim_install.ps1
+++ tools/scoop_vim_install.ps1
@@ -8,6 +8,7 @@ iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
         scoop bucket add "$_"
     }
 scoop bucket add lsq https://github.com/lsq/scoopet
+scoop update
 scoop install lsq/aria2
 scoop config aria2-retry-wait 4
 scoop config aria2-split 16
