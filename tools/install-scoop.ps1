817e6a5746a9d4cbc01999786c9fbba0ee4bc15e
lsq <lsqypj.gmail.com>
2024-09-04 02:31:20 +0800

    unpack script

--- tools/install-scoop.ps1
+++ tools/install-scoop.ps1
@@ -1,4 +1,6 @@
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+Write-host "$env:path"
+$env:path="$USERPROFILE/scoop/shims";$env:path
 "main extras games nerd-fonts nirsoft sysinternals java nonportable php versions" -split '\s+' |`
    foreach-object {
         if ($_ -eq "main") { scoop bucket rm main }
