1445599e0f4b9bc3af8a2be1b45c55ddfa90a5b8
lsq <lsqypj.gmail.com>
2024-11-17 23:04:37 +0800

    action flows

--- tools/scoop_vim_install.ps1
+++ tools/scoop_vim_install.ps1
@@ -29,25 +29,25 @@ function Get-Env {
 function Publish-Env {
     if (-not ('Win32.NativeMethods' -as [Type])) {
         Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @'
-            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-            public static extern IntPtr SendMessageTimeout(
-                    IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
-                    uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
-        '@
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+public static extern IntPtr SendMessageTimeout(
+    IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
+    uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+'@
     }
 
     $HWND_BROADCAST = [IntPtr] 0xffff
-        $WM_SETTINGCHANGE = 0x1a
-        $result = [UIntPtr]::Zero
+    $WM_SETTINGCHANGE = 0x1a
+    $result = [UIntPtr]::Zero
 
-        [Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST,
-                $WM_SETTINGCHANGE,
-                [UIntPtr]::Zero,
-                'Environment',
-                2,
-                5000,
-                [ref] $result
-                ) | Out-Null
+    [Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST,
+            $WM_SETTINGCHANGE,
+            [UIntPtr]::Zero,
+            'Environment',
+            2,
+            5000,
+            [ref] $result
+            ) | Out-Null
 }
 
 function Write-Env {
