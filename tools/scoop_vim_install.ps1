125d35bcff2b6cf3068ff548e21d17af86cf41bb
lsq <lsqypj.gmail.com>
2024-11-18 01:06:10 +0800

    action flows

--- tools/scoop_vim_install.ps1
+++ tools/scoop_vim_install.ps1
@@ -3,7 +3,7 @@ function aria2Download {
     param(
             [String] $url,
             [String] $dir,
-            [String] $out
+            [String] $outhttps://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.3.6-1/rubyinstaller-3.3.6-1-x64.7z
          )
 
         aria2c -d $url -o $out "--user-agent=Scoop/1.0 (+http://scoop.sh/) PowerShell/7.5 (Windows NT 10.0; Win64; x64; Core)" --allow-overwrite=true --auto-file-renaming=false --retry-wait=4 --split=16 --max-connection-per-server=16 --min-split-size=4M --console-log-level=warn --enable-color=false --no-conf=true --follow-metalink=true --metalink-preferred-protocol=https  --continue --summary-interval=0 --auto-save-interval=1  $url
@@ -85,7 +85,8 @@ C:\msys64\usr\bin\bash.exe -lc "pacman --noconfirm -Sy mingw-w64-ucrt-x86_64-ari
 . $PSScriptRoot\install-scoop.ps1
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
 ni $HOME/vimfiles/autoload/plug.vim -Force
-scoop install lsq/aria2
+#scoop install lsq/aria2
+scoop install aria2
 scoop config aria2-retry-wait 4
 scoop config aria2-split 16
 scoop config aria2-max-connection-per-server 16
