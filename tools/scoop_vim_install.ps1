f2c04e5237f274b4ba7038c1dd911d97e10df860
lsq <lsqypj.gmail.com>
2024-11-18 04:30:27 +0800

    action flows

--- tools/scoop_vim_install.ps1
+++ tools/scoop_vim_install.ps1
@@ -199,8 +199,13 @@ if ($env:GITHUB_ACTIONS -eq $null) {
 }
 else {
     $build_folder=$env:GITHUB_WORKSPACE
+    Write-Host $env:GITHUB_PATH
+    Write-Host $(cat $env:GITHUB_PATH)
+    echo "$env:USERPROFILE\scoop\shims;" >> $env:GITHUB_PATH
+    echo "$rubyhome\bin;$rubyhome\gems\bin;" >> $env:GITHUB_PATH
     Write-Host $(cat $env:GITHUB_PATH)
 }
+echo $build_folder
 sed.exe -r -i "s/(ci.ri2::ruby).*(:.*)/\\1$rver\\2/" $build_folder\tools\vim-build.sh
 
 #$env:USER_PATH=[Environment]::GetEnvironmentVariable("PATH", "User") 
