f288fcfa9c201a7192b121d8c9bedd0faf3f30f6
lsq <lsqypj.gmail.com>
2024-11-20 20:35:58 +0800

    add ucrt64 runtime test

--- tools/ucrt64-runtime.ps1
+++ tools/ucrt64-runtime.ps1
@@ -12,5 +12,6 @@ $env:MSYSTEM = 'msys'  # Start a 64 bit Mingw environment
 basedir="$1"
 cd "$basedir" || exit 1
 makepkg -sfL
-'@  | Out-File -FilePath  ci-build.sh -Encoding UTF8
-C:\msys64\usr\bin\bash -lc "./ci-build.sh $PSScriptRoot"
+'@  | Out-File -FilePath  ci-build.sh -Encoding utf8NoBOM
+$cygPath = $(cygpath -u $PSScriptRoot)
+C:\msys64\usr\bin\bash -lc "./ci-build.sh $cygPath"
