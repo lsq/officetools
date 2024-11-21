91e3466aaefc059f80f914f35a807ee8f82d8c89
lsq <lsqypj.gmail.com>
2024-11-21 21:32:32 +0800

    add zenity build

--- tools/build-zenity.sh
+++ tools/build-zenity.sh
@@ -7,7 +7,7 @@ mv usr ucrt64
 cp -r ucrt64 /
 rm -rf /ucrt64/include/regex.h
 pacman --noconfirm --sync --needed  mingw-w64-ucrt-x86_64-gettext-runtime \
-mingw-w64-ucrt-x86_64-gettext-tools mingw-w64-ucrt-x86_64-gcc help2man mingw-w64-ucrt-x86_64-libadwaita mingw-w64-ucrt-x86_64-meson\
+mingw-w64-ucrt-x86_64-gettext-tools mingw-w64-ucrt-x86_64-gcc help2man mingw-w64-ucrt-x86_64-libadwaita mingw-w64-ucrt-x86_64-meson \
 mingw-w64-ucrt-x86_64-gtk4 mingw-w64-ucrt-x86_64-yelp-tools --overwrite '*' 
 git clone https://github.com/GNOME/zenity.git
 cd zenity || exit 1
