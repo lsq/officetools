e281a952e391328580209a80886fb0ab6d94bf58
lsq <lsqypj.gmail.com>
2024-11-23 23:49:47 +0800

    add github repos download and upload to wormhole.app

--- tools/dl_github_repo.sh
+++ tools/dl_github_repo.sh
@@ -1,10 +1,12 @@
 set -x
+basedir=$(realpath "${0%/*}")
 
 git clone https://github.com/Mimickal/wormhole-cli.git
 cd wormhole-cli
 npm ci
 npm install -g
 
-[ command -v wormhole-cli ] || exit 1
+cd "$basedir"
+command -v wormhole-cli >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
 . ./downGithubRep.sh
 downGithubRep https://github.com/UIZorrot/Quenching-Assets.git
