738ed3fa2d1293dfcd92838aaa996a8063fae9c7
lsq <lsqypj.gmail.com>
2025-02-21 15:21:02 +0800

    update build script

--- tools/vim-build.sh
+++ tools/vim-build.sh
@@ -1,22 +1,35 @@
 set -x
-basedir=$(realpath "${0%/*}")
-echo "MSYSTEM:$MSYSTEM\nMINGW_PACKAGE_PREFIX:$MINGW_PACKAGE_PREFIX"
+function exportReplace {
+    local filePath
+
+    if [[ "$#" -lt 2 ]]; then
+        echo "Please provide at least two parameters."
+        exit 1
+    fi
+
+    local filePath="$1"
+    shift
+    while [ "$#" -gt 0 ]; do
+        sed -i 's|\${\?'"$1"'}\?|'"${!1}"'|' "$filePath"
+        shift
+    done
+}
+realpath=$(realpath "$0")
+basedir="${realpath%/*}"
+echo "$MSYSTEM"
 pacman --noconfirm --sync --needed pactoys
 pacman-key --recv-keys BE8BF1C5
 pacman-key --lsign-key BE8BF1C5
-pacman-key --recv-keys 30B77F3A
-pacman-key --lsign-key 30B77F3A
 pacman-key --recv-keys AAE32BA7
 pacman-key --lsign-key AAE32BA7
 repman add ci.ri2 "https://github.com/oneclick/rubyinstaller2-packages/releases/download/ci.ri2"
 pacman -Syuu --noconfirm
 #pacman -Sy --needed --noconfirm "ruby$rubyversion"
 pacboy sync --needed --noconfirm ed:
-pacboy sync --needed --noconfirm lua:u
-pacboy sync --needed --noconfirm jq:u
-pacboy sync --needed --noconfirm mingw-w64-ucrt-x86_64-libsodium
-pacboy sync --needed --noconfirm mingw-w64-ucrt-x86_64-tcl
-#pacboy sync --needed --noconfirm ci.ri2::ruby33:u
+pacboy sync --needed --noconfirm lua:p
+pacboy sync --needed --noconfirm jq:p
+pacboy sync --needed --noconfirm libsodium:p
+pacboy sync --needed --noconfirm ci.ri2::ruby32:p
 #cd ./vim
 #MINGW_ARCH=msys makepkg-mingw --cleanbuild --syncdeps --force --noconfirm
 #cd $APPVEYOR_BUILD_FOLDER/tools/vim
@@ -28,7 +41,8 @@ echo "$PATH"
 #export PATH=$rubyhome/bin:$PATH
 #ridk.cmd install
 #echo $PATH
-ls "$rubyhome"/bin/
+#ls "$rubyhome"/bin/
+cp PKGBUILD.var PKGBUILD
 rbpat=$(which ruby)
 rbdir=${rbpat%/*}
 rubyhm=${rbdir%/*}
@@ -39,7 +53,7 @@ rubyapiver=$(ruby -v | sed -r -n 's/.* (([0-9]{1,2})\.([0-9]{1,2})\.)[0-9]{1,2} 
 #sed -i "s|RUBY_API_VER_LONG=3.2.0|RUBY_API_VER_LONG=${rubyapiver}|" PKGBUILD
 sed -n 's/\r//p' PKGBUILD
 sed -i -r 's/(ruby)[^>]*(>=)([^"]*)/\1'$rubyversion'\2'$rubyapiver'/' PKGBUILD
-export rubyversion rubyapiver rubyhm
+exportReplace PKGBUILD rubyversion rubyapiver rubyhm
 
 pythonver=$(sed 's/\x0d\x0a//' <<<$(powershell '$webc=(iwr https://www.python.org/downloads/windows).content; $mstatus = $webc -match "Latest Python \d Release - Python (?<version>[\d.]+)"; $Matches["version"]'))
 #pypat=$(which python3)
@@ -56,7 +70,7 @@ else
     pyapiver=${pymajor}.$((pyminor - 1))
     pacboy find "python${pyapiver}:p" || { echo "python $pyapiver not find" && exit 1; }
 fi
-export pyversion pyapiver
+exportReplace PKGBUILD pyversion pyapiver
 #sed -n 's/\r//p' PKGBUILD
 
 luaversion=$(lua -v | sed -r -n 's/.*(([0-9]{1,2})\.([0-9]{1,2})\.)[0-9]{1,2}.*/\2\3/p')
@@ -64,7 +78,7 @@ tclshversionlong=$(tclsh - <<<'puts $tcl_patchLevel')
 tclversion=$(echo ${tclshversionlong} | sed -r -n 's/.*(([0-9]{1,2})\.([0-9]{1,2})\.)[0-9]{1,2}.*/\2\3/p')
 tclapiver=$(echo ${tclshversionlong} | sed -r -n 's/.*(([0-9]{1,2})\.([0-9]{1,2}))\.[0-9]{1,2}.*/\1/p')
 perlversion=$(perl -v | sed -r -n 's/.*(([0-9]{1,2})\.([0-9]{1,2})\.)[0-9]{1,2}.*/\2\3/p')
-export luaversion perlversion tclversion tclapiver
+exportReplace PKGBUILD luaversion perlversion tclversion tclapiver
 
 racketBin=$(which racket)
 if [[ ${racketBin} =~ "shim" ]]; then
@@ -79,8 +93,7 @@ echo ${racketlib}
 mzlib=$(ls ${racketlib}/libracket*.dll)
 mzVer=$(sed 's|libracket||;s|\.dll||' <<<$(basename "$mzlib"))
 echo $mzVer
-export racketHome
-export mzVer
+exportReplace PKGBUILD racketHome mzVer
 
 pkgver() {
     local ver
@@ -104,9 +117,13 @@ if [ -n "$newerVer" ] && [ $(vercmp "$olderVer" "$newerVer") -ne 0 ]; then
     #chsm=$(makepkg-mingw -oeg |sed ':t;N;$! bt;s/\n/|/g;s/\x27/#/g;')
     #sed -i '\~^sha256sums=~{:t N;s~.*\x27)~'"$chsm"'~;T t;s~#~\x27~g;s~|~\n~g;}' PKGBUILD
     #printf '%s\n' "g/1/s//$chsm/" 'wq' | ed -s sed.txt
+    
+else
+    exit 1
 fi
-export MSYS=winsymlinks:lnk
+#export MSYS=winsymlinks:lnk
 #MINGW_ARCH=ucrt64 makepkg-mingw -eo
+## https://github.com/msys2/MSYS2-packages/issues/1216
 MINGW_ARCH=ucrt64 makepkg-mingw -sLf --noconfirm
 libsodiumVer=$(pacman -Qi mingw-w64-ucrt-x86_64-libsodium | grep -Po '^(版本|Version)\s*: \K.+')
 VIMVER="$newerVer"
@@ -114,7 +131,7 @@ VIMVERMAJOR=$(awk -F'.' '{print $1$2}' <<<"$newerVer")
 interfaceInfo=$(cat src/vim-"${VIMVER}"/src/if_ver.txt | sed -r -n 's/\s*(.*):\s*$/\* \1:/;3!p')
 if [ -z "$APPVEYOR_REPO_NAME" ]; then
     CI_REPO_NAME=$GITHUB_REPOSITORY
-    CI_REPO_TAG_NAME="vim${VIMVER}"
+    CI_REPO_TAG_NAME="${VIMVER}"
     echo "tagName=${CI_REPO_TAG_NAME}" >>$GITHUB_ENV
 else
     CI_REPO_NAME=$APPVEYOR_REPO_NAME
@@ -124,9 +141,9 @@ URL="https://github.com/$CI_REPO_NAME/releases/download"
 releaseLog="[![${CI_REPO_NAME}](https://img.shields.io/github/downloads/${CI_REPO_NAME}/${CI_REPO_TAG_NAME}/total.svg)](https://github.com/${CI_REPO_NAME}/releases/tag/${CI_REPO_TAG_NAME})
 ### Files:
 #### :unlock: Unsigned Files:
-* [![gvim_${VIMVER}_x64.zip](https://img.shields.io/github/downloads/${CI_REPO_NAME}/${CI_REPO_TAG_NAME}/gvim_${VIMVER}_x64.zip.svg?label=downloads&logo=vim)](${URL}/${CI_REPO_TAG_NAME}/gvim_${VIMVER}_x86_64.zip)
+* [![gvim_${VIMVER}_x86_64.zip](https://img.shields.io/github/downloads/${CI_REPO_NAME}/${CI_REPO_TAG_NAME}/gvim_${VIMVER}_x86_64.zip.svg?label=downloads&logo=vim)](${URL}/${CI_REPO_TAG_NAME}/gvim_${VIMVER}_x86_64.zip)
         64-bit zip archive
-* [![gvim_${VIMVER}_x64.zip](https://img.shields.io/github/downloads/${CI_REPO_NAME}/${CI_REPO_TAG_NAME}/mingw-w64-ucrt-x86_64-vim${VIMVERMAJOR}-${VIMVER}-1-any.pkg.tar.zst.svg?label=downloads&logo=vim)](${URL}/${CI_REPO_TAG_NAME}/mingw-w64-ucrt-x86_64-vim${VIMVERMAJOR}-${VIMVER}-1-any.pkg.tar.zst)
+* [![gvim_${VIMVER}_x86_64.zip](https://img.shields.io/github/downloads/${CI_REPO_NAME}/${CI_REPO_TAG_NAME}/mingw-w64-ucrt-x86_64-vim${VIMVERMAJOR}-${VIMVER}-1-any.pkg.tar.zst.svg?label=downloads&logo=vim)](${URL}/${CI_REPO_TAG_NAME}/mingw-w64-ucrt-x86_64-vim${VIMVERMAJOR}-${VIMVER}-1-any.pkg.tar.zst)
         64-bit ucrt installer archive
 
 <details>
